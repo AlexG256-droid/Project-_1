6d08e2a2db9e160a2ab2d8d33e6d74475ec90007
Alex Gutting <gutting.a@northeastern.edu>
2026-05-27 17:32:55 -0400

    Update topic_distribution.js

--- JavaScript/topic_distribution.js
+++ JavaScript/topic_distribution.js
@@ -3,9 +3,9 @@ const imageChange2 = document.querySelector("img");
 
 imageChange2.addEventListener("click", () => {
   const mySrc = imageChange2.getAttribute("src");
-  if (mySrc === "../Images/Topic_Distribution.png") {
-    imageChange2.setAttribute("src", "../Images/Topic_Distribution_2.png");
+  if (mySrc === "Images/Topic_Distribution.png") {
+    imageChange2.setAttribute("src", "Images/Topic_Distribution_2.png");
   } else {
-    imageChange2.setAttribute("src", "../Images/Topic_Distribution.png");
+    imageChange2.setAttribute("src", "Images/Topic_Distribution.png");
   }
 });
